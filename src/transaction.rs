ab687989c5eb75264586094b0f623d14c5f4d548
Leonardo Barbosa <leonardobarbosa@Leonardos-MacBook-Pro-2.local>
2024-04-07 00:40:29 -0300

    add transaction

--- src/transaction.rs
+++ src/transaction.rs
@@ -2,7 +2,9 @@ use prost::Message;
 use tonic::{Request, Response, Status};
 use transaction::transaction_service_server::TransactionService;
 use std::sync::{Arc, Mutex};
-
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::fs;
 use self::btree::BTree;
 
 #[path = "./btree.rs"]
@@ -27,7 +29,50 @@ impl Transaction {
 #[tonic::async_trait]
 impl TransactionService for Transaction {
     async fn add_transaction(&self, request: Request<transaction::AddTransactionRequest>) -> Result<Response<transaction::Empty>, Status> {
+        
         let request = request.into_inner();
+        let transaction = request.transaction.unwrap();
+        let block_hash = request.block_hash;
+        let db = self.db.lock().unwrap();
+        let id = db.get(block_hash.clone());
+
+        if id.is_none() {
+            return Err(Status::not_found("Block not found"));
+        }
+
+        let mut buf_transaction = vec![];
+        transaction.encode(&mut buf_transaction).unwrap();
+
+        if buf_transaction.len() > 2u32.pow(16).try_into().unwrap() {
+            return Err(Status::invalid_argument("Transaction too large"));
+        }
+
+        let transaction_size_buf = buf_transaction.len().to_be_bytes();
+        let first_two_bytes_of_transaction_size = &transaction_size_buf.get(transaction_size_buf.len() - 2..).unwrap();
+
+        let mut buf = Vec::new();
+        buf.extend_from_slice(&first_two_bytes_of_transaction_size);
+        buf.extend_from_slice(&buf_transaction);  
+
+        let id = id.unwrap();
+        let filename = format!("blocks/{}", String::from_utf8(id).unwrap());
+        let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(filename.clone())
+        .unwrap();
+
+        file.write_all(&buf).unwrap();
+
+        let metadata = fs::metadata(filename.clone())?;
+        let shift = metadata.len();
+        
+        let mut buf = Vec::new();
+        buf.extend_from_slice(&shift.to_be_bytes().to_vec());
+        buf.extend_from_slice(&block_hash.as_bytes().to_vec());
+        
+        db.insert(transaction.hash.clone(), buf);
+
         Ok(Response::new(transaction::Empty {}))
     }
 
