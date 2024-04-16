29cbfb9f40f7b64a89152684f632e3d925e15cc3
Leonardo Barbosa <leonardobarbosa@Leonardos-MacBook-Pro-2.local>
2024-04-15 22:09:04 -0300

    feat/add transaction and find transaction by hash

--- src/transaction.rs
+++ src/transaction.rs
@@ -5,6 +5,7 @@ use std::sync::{Arc, Mutex};
 use std::fs::OpenOptions;
 use std::io::prelude::*;
 use std::fs;
+
 use self::btree::BTree;
 
 #[path = "./btree.rs"]
@@ -40,6 +41,15 @@ impl TransactionService for Transaction {
             return Err(Status::not_found("Block not found"));
         }
 
+        let exists_transaction = {
+            let buf = db.get(transaction.hash.clone());
+            buf.is_some()
+        };
+
+        if exists_transaction {
+            return Err(Status::already_exists("Transaction already exists"));
+        }
+
         let mut buf_transaction = vec![];
         transaction.encode(&mut buf_transaction).unwrap();
 
@@ -56,6 +66,10 @@ impl TransactionService for Transaction {
 
         let id = id.unwrap();
         let filename = format!("blocks/{}", String::from_utf8(id).unwrap());
+
+        let metadata = fs::metadata(filename.clone())?;
+        let shift = metadata.len();
+
         let mut file = OpenOptions::new()
         .write(true)
         .append(true)
@@ -63,36 +77,47 @@ impl TransactionService for Transaction {
         .unwrap();
 
         file.write_all(&buf).unwrap();
-
-        let metadata = fs::metadata(filename.clone())?;
-        let shift = metadata.len();
         
         let mut buf = Vec::new();
         buf.extend_from_slice(&shift.to_be_bytes().to_vec());
         buf.extend_from_slice(&block_hash.as_bytes().to_vec());
-        
+        println!("db: {:?}", buf);
+
         db.insert(transaction.hash.clone(), buf);
 
         Ok(Response::new(transaction::Empty {}))
     }
 
     async fn find_transaction_by_hash(&self, request: Request<transaction::FindTransactionByHashRequest>) -> Result<Response<transaction::Transaction>, Status> {
-        let transaction = transaction::Transaction {
-            index: 0,
-            previous_hash: String::default(),
-            timestamp: 0,
-            data: String::default(),
-            signature: String::default(),
-            nonce: 0,
-            identification: String::default(),
-            hash: String::default(),
-        };
+      let request = request.into_inner();
+      let db = self.db.lock().unwrap();
 
-        let mut buf = vec![];
-        transaction.encode(&mut buf).unwrap();
-        let decoded = transaction::Transaction::decode(&buf[..]).unwrap();
 
-        Ok(Response::new(decoded))
+      let buf = db.get(request.hash.clone());
+      if buf.is_none() {
+          return Err(Status::not_found("Transaction not found"));
+      }
+
+      
+      let buf = buf.unwrap();
+      let block_hash = String::from_utf8(buf.get(8..).unwrap().to_vec()).unwrap();
+
+
+      let id = db.get(block_hash.clone());
+
+      if id.is_none() {
+          return Err(Status::not_found("Block not found"));
+      }
+
+      let shift = u64::from_be_bytes(buf.get(0..8).unwrap().try_into().unwrap());
+      let filename = format!("blocks/{}", String::from_utf8(id.unwrap()).unwrap());
+      let buf = fs::read(filename).unwrap();
+      
+      let transaction_size = u16::from_be_bytes([buf[shift as usize], buf[shift as usize + 1]]);
+      println!("transaction_size: {}", transaction_size);
+      let transaction = transaction::Transaction::decode(&buf[shift as usize + 2..transaction_size as usize + shift as usize + 2]).unwrap();
+
+      Ok(Response::new(transaction))
     }
 
     async fn exists_transaction_on_block(&self, request: Request<transaction::ExistsTransactionOnBlockRequet>) -> Result<Response<transaction::ExistsTransactionOnBlockReply>, Status> {
