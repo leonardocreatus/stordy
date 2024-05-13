6db4180aaa7288b6a895842284420060009e7c34
sillysecret <j.kriger002@edu.pucrs.br>
2024-05-13 16:29:20 -0300

    adicionando comentarios para revisao

--- src/transaction.rs
+++ src/transaction.rs
@@ -33,50 +33,43 @@ impl Transaction {
 
 #[tonic::async_trait]
 impl TransactionService for Transaction {
-
-
   async fn find_last_transaction(&self, request: Request<transaction::GetLastRequest>) -> Result<Response<transaction::Transaction>, Status>{
     let request = request.into_inner();
     let block_public_key = request.block_public_key;
     let db_block = self.db_block.lock().unwrap();
     let id = db_block.get(block_public_key);
-    
+    //verifica se o bloco existe
     if id.is_none() {
       return Err(Status::not_found("Block not found"));
-  }
-
+    }
     let id = id.unwrap();
-
     let filename = format!("blocks/{}", String::from_utf8(id).unwrap());
-
-    
+    //le o arquivo do block e coloca no buffer
     let buf = match fs::read(&filename) {
         Ok(buf) => buf,
         Err(_) => return Err(Status::internal("Error reading block file")),
     };
-
-   
+    //verifica se o buffer esta vazio ou seja, se nao tem transacoes no bloco
     if buf.is_empty() {
         return Err(Status::not_found("No transactions in the block"));
     }
-
     let mut offset = 0;
     let mut last_transaction_offset = 0;
-
-    // Percorrer o bloco
+    //percorre sobre o buffer para pegar o shift nessessario para a ultima transacao 
     while offset < buf.len() {
-       
+        //pega o tamanho da transacao que esta no shift atual e depois usa pra calcular o proximo shift
         let transaction_size = u16::from_be_bytes([buf[offset], buf[offset + 1]]); 
         last_transaction_offset = offset;
+        //shift atual usado so no while para pegar o proximo shift ate encontrar o final
         offset += 2 + transaction_size as usize;
     }
-
-    // Decodificar a última transação encontrada
+    //calcula o size da transacao pegando os 2 bytes do shift que conseguimos da ultima transacao
     let last_transaction_size = u16::from_be_bytes([
         buf[last_transaction_offset],
         buf[last_transaction_offset + 1],
     ]);
-    let last_transaction = transaction::Transaction::decode(&buf[last_transaction_offset + 2..last_transaction_offset + 2 + last_transaction_size as usize])
+    //calcula o range do buffer para pegar do campo do shift + 2 ate o shift + 2 + tamanho da transacao. Assim conseguindo a transacao inteira e depois decodifica
+    let last_transaction = transaction::Transaction::decode(&buf[last_transaction_offset + 2..(last_transaction_offset + 2 + last_transaction_size as usize)])
         .unwrap();
 
     Ok(Response::new(last_transaction))
