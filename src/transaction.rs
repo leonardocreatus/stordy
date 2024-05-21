4b930b70e66db2236e5ce61d48a24f436932624f
sillysecret <j.kriger002@edu.pucrs.br>
2024-05-21 09:24:28 -0300

    mudando para implementar com o seek

--- src/transaction.rs
+++ src/transaction.rs
@@ -5,6 +5,8 @@ use std::sync::{Arc, Mutex};
 use std::fs::OpenOptions;
 use std::io::prelude::*;
 use std::fs;
+use std::io;
+use std::io::prelude::*;
 
 use self::btree::BTree;
 
@@ -133,26 +135,58 @@ impl TransactionService for Transaction {
 
     async fn find_last_transaction(&self, request: Request<transaction::FindLastTransactionRequest>) -> Result<Response<transaction::Transaction>, Status> {
         
-        // let db_transaction = self.db_transaction.lock().unwrap();
-        let db_block = self.db_block.lock().unwrap();
-        let block_public_key = request.into_inner().block_public_key;
-        let block_id = db_block.get(block_public_key);
+      let db_block = self.db_block.lock().unwrap();
+      let block_public_key = request.into_inner().block_public_key;
+      let block_id = db_block.get(block_public_key);
 
-        if block_id.is_none() {
-            return Err(Status::not_found("Block not found"));
-        }
+      if block_id.is_none() {
+          return Err(Status::not_found("Block not found"));
+      }
+      // let block_id = block_id.unwrap();
+      // let buf = fs::read(format!("blocks/{}", String::from_utf8(block_id).unwrap())).unwrap();
+      // let size_last_transaction = u16::from_be_bytes([buf[buf.len() - 2], buf[buf.len() - 1]]);
+      // let shift = buf.len() - 2 - size_last_transaction as usize;
 
-        let block_id = block_id.unwrap();
-        let buf = fs::read(format!("blocks/{}", String::from_utf8(block_id).unwrap())).unwrap();
-        let size_last_transaction = u16::from_be_bytes([buf[buf.len() - 2], buf[buf.len() - 1]]);
-        let shift = buf.len() - 2 - size_last_transaction as usize;
+      // let buf = buf.get(shift..buf.len() - 2).unwrap();
+      // let transaction = transaction::Transaction::decode(buf).unwrap();
+      //Ok(Response::new(transaction))
+      let filename = format!(
+          "blocks/{}",
+          String::from_utf8(block_id.clone().expect("REASON")).unwrap()
+      );
 
-        let buf = buf.get(shift..buf.len() - 2).unwrap();
-        let transaction = transaction::Transaction::decode(buf).unwrap();
+      //texto da documentação do FILE
+      // An object providing access to an open file on the filesystem.
+      // An instance of a File can be read and/or written depending on what options it was opened with.
+      //Files also implement Seek to alter the logical cursor that the file contains internally.
+      // Files are automatically closed when they go out of scope. Errors detected on closing are ignored by the implementation of Drop.
+      //Use the method sync_all if these errors must be manually handled.
+      //****File does not buffer reads and writes****.
+      //For efficiency, consider wrapping the file in a BufReader or BufWriter when performing many small read or write calls,
+      // unless unbuffered reads and writes are required.
+      let mut arquivo = fs::File::open(&filename)?;
+      //pega o tamanho do arquivo para poder ver quanto devemos voltar para pegar a ultima transacao
+      let tamanho_arquivo = arquivo.metadata()?.len();
+      // move o cursor para 2 bytes antes do final do arquivo
+      arquivo.seek(io::SeekFrom::End(-2))?;
+      let mut buffer = [0; 2];
+      // le os ultimos 2 bytes do arquivo para o buffer
+      arquivo.read_exact(&mut buffer)?;
+      let size_last_transaction = u16::from_be_bytes(buffer);
+      // pega o offset/shift da transacao
+      let shift = (tamanho_arquivo - 2 - size_last_transaction as u64).max(0);
+      // mcove o ponteiro do arquivo para o inicio da transacao
+      arquivo.seek(io::SeekFrom::Start(shift))?;
+      // cria um buffer do tamanho da  transação
+      let mut buffer = vec![0; size_last_transaction as usize];
+      // le a ultima transacao para o buffer
+      arquivo.read_exact(&mut buffer)?;
 
-        
-
-        Ok(Response::new(transaction))
+      match transaction::Transaction::decode(&*buffer) {
+          Ok(transaction) => Ok(Response::new(transaction)),
+          Err(_) => Err(Status::internal("Failed to decode transaction")),
+      }
+      
     }
 
     async fn find_transaction_by_hash(&self, request: Request<transaction::FindTransactionByHashRequest>) -> Result<Response<transaction::Transaction>, Status> {
